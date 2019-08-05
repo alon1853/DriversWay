118e64769600d173a3e90ed85f918416f6ec2f04
Alon <alon1853@gmail.com>
2019-08-05 23:45:24 +0300

    Add ability to sort drivers by name or age

--- src/stores/drivers-store.js
+++ src/stores/drivers-store.js
@@ -6,9 +6,12 @@ class ObservableDriversStore {
     @observable drivers = [];
     @observable driversMap = new Map();
     @observable driverFilter = '';
+    @observable sortBy = '';
+    @observable sortAscending = true;
 
     constructor() {
         this.fetchDrivers();
+        this.setSortByAge();
     }
 
     @action initDriversMap() {
@@ -41,6 +44,18 @@ class ObservableDriversStore {
     @computed get filteredDrivers() {
         const drivers = values(this.driversMap);
 
+        switch (this.sortBy) {
+            case 'age': {
+                drivers.sort(this.sortByAge.bind(this));
+                break;
+            }
+            case 'name':
+            default: {
+                drivers.sort(this.sortByName.bind(this));
+                break;
+            }
+        }
+
         if (this.driverFilter) {
             return drivers.filter(
                 (driver) => driver.name.toLowerCase().includes(this.driverFilter.toLowerCase())
@@ -62,6 +77,50 @@ class ObservableDriversStore {
 
     @action setDriverFilter(filter) {
         this.driverFilter = filter;
+    }
+
+    @action toggleSortDirection() {
+        this.sortAscending = !this.sortAscending;
+    }
+
+    @action setSortByName() {
+        this.sortBy = 'name';
+    }
+
+    @action setSortByAge() {
+        this.sortBy = 'age';
+    }
+
+    sortByName(driverA, driverB) {
+        const nameA = driverA.name.toUpperCase();
+        const nameB = driverB.name.toUpperCase();
+
+        if (nameA < nameB) {
+            if (this.sortAscending) {
+                return -1;
+            }
+
+            return 1;
+        }
+        if (nameA > nameB) {
+
+            if (this.sortAscending) {
+                return 1;
+            }
+            return -1;
+        }
+
+        return 0;
+    }
+
+    sortByAge(driverA, driverB) {
+        const result = driverA.age - driverB.age;
+
+        if (!this.sortAscending) {
+            return result * -1;
+        }
+
+        return result;
     }
 }
 
